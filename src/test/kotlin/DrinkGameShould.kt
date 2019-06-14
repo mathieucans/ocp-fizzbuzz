87c1280e9a27cfdc2ef42e085772fe9a9ed5eea4
CANS Mathieu <mathieu.cans@orange.com>
2019-06-14 11:19:34 +0200

    ready for bang

--- src/test/kotlin/DrinkGameShould.kt
+++ src/test/kotlin/DrinkGameShould.kt
@@ -7,7 +7,15 @@ class DrinkGameShould {
     @ParameterizedTest
     @CsvSource(
         "1, 1",
-        "2, 2")
+        "2, 2",
+        "3, fizz",
+        "4, 4",
+        "5, buzz",
+        "7, bang",
+        "9, fizz",
+        "15, fizzbuzz",
+        "150, fizzbuzz")
+
     fun say_the_number(number:Int, say:String) {
         assertThat(FizzBuzzFactory().create(number).say()).isEqualTo(say)
     }
--- src/test/kotlin/FizzBuzzFactory.kt
+++ src/test/kotlin/FizzBuzzFactory.kt
@@ -1,14 +1,64 @@
 class FizzBuzzFactory {
-    fun create(number: Int): SayOne {
-        return SayOne()
+    fun create(number: Int): Say {
+        return SayAll(listOf(
+            Couple(3, SayFizz()),
+            Couple(5, SayBuzz()),
+            Couple(7, SayBang())
+        ).filter{ it.predicate(number)}
+            .map{ it.say}
+            .ifEmpty {listOf(SayIdentity(number)) })
+    }
+
+
+}
+
+class SayBang : Say {
+    override fun say(): String {
+        return "bang"
     }
 
 }
 
-class SayOne
-{
-    fun say(): String{
-        return "1"
+class SayAll(val filter: List<Say>) : Say {
+    override fun say(): String {
+        return filter.map { it.say() }.reduce { acc, say ->  acc + say}
+    }
+
+}
+
+class SayFizzBuzz : Say {
+    override fun say(): String {
+        return "fizzbuzz"
+    }
+
+}
+
+class Couple(val modulo: Int, val say: Say) {
+    fun predicate(number: Int): Boolean {
+        return number % modulo == 0
+    }
+
+}
+
+interface Say {
+    fun say(): String
+}
+
+class SayIdentity(val number:Int) : Say {
+
+    override fun say(): String {
+        return number.toString()
+    }
+}
+
+class SayFizz : Say {
+    override fun say(): String {
+        return "fizz"
+    }
+}
+class SayBuzz : Say {
+    override fun say(): String {
+        return "buzz"
     }
 }
 
